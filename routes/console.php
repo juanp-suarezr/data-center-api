76030838bca7c6e102d54892ae3736708f19d730
juanp-suarezr <juanp-suarezr@users.noreply.github.com>
2026-07-06 02:13:32 -0500

    feat: add inspire and queue:work artisan commands to console routes

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('queue:work --stop-when-empty --tries=3', function () {
+    $this->exec('queue:work --stop-when-empty --tries=3');
+
+    $this->info('Starting queue worker...');
+})->purpose('Start the queue worker');
+
